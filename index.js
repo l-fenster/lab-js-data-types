de9d39335c55ebbf596f245ba80e099193073a80
Leah Fenster <leahefen@gmail.com>
2024-07-10 16:25:54 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -22,11 +22,13 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const javA = part1.substr(0, (part1.length - 1)) + (part1.charAt(part1.length - 1)).toUpperCase();
+const scripT = part2.substr(0, (part2.length - 1)) + (part2.charAt(part2.length - 1)).toUpperCase();
+const result = javA + scripT;
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result);
 
 
 /*******************************************
@@ -35,11 +37,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = Math.round((billTotal * .15) * 100) / 100
 
 // Print out the tipAmount
 
-
+console.log(tipAmount);
 
 
 /*******************************************
@@ -47,11 +49,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +63,17 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
-
-const expression2 = a || b;
-
-const expression3 = !a && b;
-
-const expression4 = !(a && b);
-
-const expression5 = !a || !b;
-
-const expression6 = !(a || b);
-
-const expression7 = a && a;
+const expression1 = a && b; //guess: false
+console.log(expression1);
+const expression2 = a || b; //guess: true
+console.log(expression2);
+const expression3 = !a && b; //guess: false
+console.log(expression3);
+const expression4 = !(a && b); //guess: true
+console.log(expression4);
+const expression5 = !a || !b; //guess: true
+console.log(expression5);
+const expression6 = !(a || b); //guess: false
+console.log(expression6);
+const expression7 = a && a; //guess: true
+console.log(expression7);
